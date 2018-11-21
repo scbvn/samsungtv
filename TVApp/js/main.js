d29481b6c64f1635638a30e4407c287b3167fbb9
Smart City <smartcity.boschvn@gmail.com>
2018-11-21 08:54:01 +0700

    update code

--- TVApp/js/main.js
+++ TVApp/js/main.js
@@ -1,4 +1,17 @@
 
+var imageMapping = {
+		"mr.duy": "images/members/duy/Duy.jpg",
+		"mrs.huong": "images/members/huong/Huong.jpg",
+		"mr.markus": "images/members/markus/Markus.jpg",
+		"mr.my": "images/members/my/My.jpg",
+		"mr.sang": "images/members/sang/Sang.jpg",
+		"mr.son": "images/members/son/Son.jpg",
+		"mr.sum": "images/members/sum/Sum.jpg",
+		"mr.trong": "images/members/trong/Trong.jpg",
+		"mr.tuan": "images/members/tuan/tuan.jpg",
+		"stranger": "images/members/unknow/firework.png"
+}
+
 window.onload = function () {
     // TODO:: Do your initialization job
 
@@ -15,7 +28,7 @@ window.onload = function () {
     
     //mqtt
     // Create a client instance
-    client = new Paho.MQTT.Client("192.168.1.31", 15675, "/ws", "clientId-12345");
+    client = new Paho.MQTT.Client("192.168.1.31", 15675, "/ws", "clientId-" + guid());
 
     console.log("client:" + client);
     
@@ -34,7 +47,7 @@ window.onload = function () {
       // Once a connection has been made, make a subscription and send a message.
       console.log("onConnect");
       var textboxTemp = document.querySelector('#textbox');
-      client.subscribe("/topic/test");
+      client.subscribe("test/detection");
 //      message = new Paho.MQTT.Message("Hello");
 //      message.destinationName = "World";
 //      client.send(message);
@@ -54,20 +67,35 @@ window.onload = function () {
       var visitor = message.payloadString;
       var box = document.querySelector('#textbox');
       box.innerHTML = visitor;
-      
-      
+        
       var subMessage = document.querySelector('#submessage');
       subMessage.innerHTML = "Have a good journey";
       
       var avarta = document.querySelector('#avarta');
-      if(message.payloadString.includes("Sum")) {
-    	  avarta.src = "images/kisimita.jpg";
-    	  avarta.style.display = 'block';
-      } else {
-    	  avarta.style.display = 'none';
-      }
+      avarta.src = imageMapping[visitor.toLowerCase()];
+	  avarta.style.display = 'block';
+	  
+	  var audio = new Audio('sounds/ding-dong.wav');
+	  audio.volume = 0.7;
+	  audio.play();
+      
+//      if(message.payloadString.includes("Sum")) {
+//    	  avarta.src = "images/kisimita.jpg";
+//    	  avarta.style.display = 'block';
+//      } else {
+//    	  avarta.style.display = 'none';
+//      }
     }
     
 };
+
+function guid() {
+	  function s4() {
+	    return Math.floor((1 + Math.random()) * 0x10000)
+	      .toString(16)
+	      .substring(1);
+	  }
+	  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}
 
 
